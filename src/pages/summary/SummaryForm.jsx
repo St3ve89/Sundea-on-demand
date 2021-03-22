adb1ddd0fad735ce27bff1ebb932f5d43cd05376
St3ve89 <istvan.acs89@gmail.com>
2021-03-22 19:55:41 +0000

    Add popover to summary form

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,12 +1,22 @@
 import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Popover from 'react-bootstrap/Popover';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 
 export default function SummaryForm() {
   const [tcChecked, setTcChecked] = useState(false);
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Content>No ice cream will actually be delivered</Popover.Content>
+    </Popover>
+  );
   const checkboxLabel = (
     <span>
-      I aggree to <span style={{ color: 'blue' }}>Terms and Conditions</span>
+      I aggree to
+      <OverlayTrigger overlay={popover} placement="right">
+        <span style={{ color: 'blue' }}>Terms and Conditions</span>
+      </OverlayTrigger>
     </span>
   );
   return (
