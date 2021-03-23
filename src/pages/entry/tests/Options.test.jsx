ac78a55c094362c3a45f43b3536e9303edbbd9ba
St3ve89 <istvan.acs89@gmail.com>
2021-03-23 15:28:16 +0000

    Add test for toppingsoptions

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -12,3 +12,21 @@ test('displays image for each scoop from the server', async () => {
   const altText = scoopImages.map((el) => el.alt);
   expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
 });
+
+test('displays image for each toppings from the server', async () => {
+  render(<Options optionType="toppings" />);
+
+  // find the images
+  const toppingImages = await screen.findAllByRole('img', {
+    name: /topping$/i,
+  });
+  expect(toppingImages).toHaveLength(3);
+
+  // confirm alt text of images
+  const altText = toppingImages.map((el) => el.alt);
+  expect(altText).toEqual([
+    'Cherries topping',
+    'M&Ms topping',
+    'Hot fudge topping',
+  ]);
+});
