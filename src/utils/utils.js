a55ecdcf5842ba70a65cdefffbd441bb27cd94a3
St3ve89 <istvan.acs89@gmail.com>
2021-03-25 19:12:46 +0000

    Create a calculateSubtotal helper function

---
+++ src/utils/utils.js
@@ -0,0 +1,10 @@
+import { pricePerItem } from '../constants';
+
+export function calculateSubtotal(optionType, optionCounts) {
+  let optionCount = 0;
+  for (const count of optionCounts[optionType].values()) {
+    optionCount += count;
+  }
+
+  return optionCount * pricePerItem[optionType];
+}
