619d1a8d6c4d8d2cb0ad75d2c682a1e6b0e69165
St3ve89 <istvan.acs89@gmail.com>
2021-03-25 19:12:25 +0000

    Create orderdetails context

---
+++ src/contexts/OrderDetails.jsx
@@ -0,0 +1,43 @@
+import React, { createContext, useEffect, useMemo, useState } from 'react';
+import calculateSubtotal from '../utils/utils';
+
+export const OrderDetails = createContext();
+
+export function OrderDetailsProvider(props) {
+  const [optionCounts, setOptionCounts] = useState({
+    scoops: new Map(),
+    toppings: new Map(),
+  });
+
+  const [totals, setTotals] = useState({
+    scoops: 0,
+    toppings: 0,
+    grandTotal: 0,
+  });
+
+  useEffect(() => {
+    const scoopsSubtotal = calculateSubtotal('scoops', optionCounts);
+    const toppingsSubtotal = calculateSubtotal('toppings', optionCounts);
+    const grandTotal = scoopsSubtotal + toppingsSubtotal;
+    setTotals({
+      scoops: scoopsSubtotal,
+      toppings: toppingsSubtotal,
+      grandTotal,
+    });
+  }, [optionCounts]);
+
+  const value = useMemo(() => {
+    function updateItemCount(itemName, newItemCount, optionType) {
+      const newOptionCounts = { ...optionCounts };
+
+      const optionCountsMap = optionCounts[optionType];
+      optionCountsMap.set(itemName, parseInt(newItemCount));
+      setOptionCounts(newOptionCounts);
+    }
+    // getter: object containing option counts for scoops and toppings, subtotals and totals
+    // setter: update options
+    return [{ ...optionCounts, totals }, updateItemCount];
+  }, [optionCounts, totals]);
+
+  return <OrderDetails.Provider value={value} {...props} />;
+}
