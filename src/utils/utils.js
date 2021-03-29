c70cbf4e02b9d76243b5dade9b69b039ade70310
St3ve89 <istvan.acs89@gmail.com>
2021-03-29 15:56:50 +0100

    Add documentation for formatCurrency

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,6 +2,7 @@ import { pricePerItem } from '../constants';
 
 export function calculateSubtotal(optionType, optionCounts) {
   let optionCount = 0;
+  console.log({ optionCounts });
   for (const count of optionCounts[optionType].values()) {
     optionCount += count;
   }
@@ -9,6 +10,20 @@ export function calculateSubtotal(optionType, optionCounts) {
   return optionCount * pricePerItem[optionType];
 }
 
+/**
+ * @function formatCurrency
+ * Format number as currency (GBP)
+ * @param {number} amount
+ * @returns {string} number formatted as currency
+ *
+ * @example
+ *  formatCurrency(0)
+ *  // => £0.00
+ *
+ * @example
+ *  formatCurrency(1.5)
+ *  // => £1.50
+ */
 export function formatCurrency(amount) {
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
