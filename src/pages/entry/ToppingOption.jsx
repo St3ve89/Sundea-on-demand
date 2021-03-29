cfa9eede9f2f196618d32295580c65643250a805
St3ve89 <istvan.acs89@gmail.com>
2021-03-29 12:20:40 +0100

    Add code for toppings

--- src/pages/entry/ToppingOption.jsx
+++ src/pages/entry/ToppingOption.jsx
@@ -1,6 +1,10 @@
 import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
 
-export default function ToppingOption({ name, imagePath }) {
+export default function ToppingOption({ name, imagePath, updateItemCount }) {
+  const handleChange = (ev) => {
+    updateItemCount(name, ev.target.checked ? 1 : 0);
+  };
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
       <img
@@ -8,6 +12,11 @@ export default function ToppingOption({ name, imagePath }) {
         src={`http://localhost:3030/${imagePath}`}
         alt={`${name} topping`}
       />
+      <Form.Group controlId={`${name}-topping-checkbox`}>
+        <Col xs="5">
+          <Form.Check onChange={handleChange} type="checkbox" label={name} />
+        </Col>
+      </Form.Group>
     </Col>
   );
 }
