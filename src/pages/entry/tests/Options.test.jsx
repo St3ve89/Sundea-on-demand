2722988270ad20cbd4b8bca5f5306d1f644e971b
St3ve89 <istvan.acs89@gmail.com>
2021-03-29 11:35:19 +0100

    Creating Custom Render to Wrap in Provider By Default

--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -1,9 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import { OrderDetailsProvider } from '../../../contexts/OrderDetails';
+import { render, screen } from '../../../test-utils/testing-library-utils';
 import Options from '../Options';
 
 test('displays image for each scoop from the server', async () => {
-  render(<Options optionType="scoops" />, { wrapper: OrderDetailsProvider });
+  render(<Options optionType="scoops" />);
 
   // find the images
   const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i });
