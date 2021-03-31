d0da705b831590726adea577dfc8bbec93e77d13
St3ve89 <istvan.acs89@gmail.com>
2021-03-31 12:16:46 +0100

    Add test for orderPahese

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -19,4 +19,7 @@ export const handlers = [
       ])
     );
   }),
+  rest.post('http://localhost:3000/order', (req, res, ctx) => {
+    return res(ctx.json({ orderNumber: 123455676 }));
+  }),
 ];
